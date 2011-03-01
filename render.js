11487839f8fd1036a5e4120df222a8adbfffc1f0
S Anand <root.node@gmail.com>
2011-03-01 10:24:38 +0000

    Refactored rendering to use a standard rendering template structure

--- render.js
+++ render.js
@@ -1,151 +1,150 @@
 var qs = require('querystring');
 var _ = require('underscore')._;
 
+// An accumulative template
+// ------------------------------------------------------------------------------------------------------------------------------------------------------
+// Usage:
+// var t = new _.Template({'b': '<b><%= text %></b> ', 'i': '<i><%= text %></i>'},
+//                        global_data).t;     // <-- don't forget the last 't'
+// t('b', {text:'bold'});
+// t('i', {text:'italics'});
+// t('b', {text:'bold again'}).join('') // --> <b>bold</b><i>italics</i>
+//
+// Every time t(templatename, data) is called, it does the following:
+//
+// 1. Extends the global data object with the new data
+// 2. Generates the templatename using the global data
+// 3. Returns the results array
+_.Template = function(templates, global) {
+  var that = this;
+  that.templates = templates;
+  that.global = global || {};
+  that.result = [];
+  that.t = function(name, data) {
+    if (data) { _.extend(global, data); }
+    if (name && that.templates[name]) { that.result.push(_.template(that.templates[name], global)); }
+    return that.result;
+  };
+};
+
 // Render a home page of an app
 // ------------------------------------------------------------------------------------------------------------------------------------------------------
 this.home = function(app) {
-  var html = [];
+  var t = new _.Template({
+    'form_start':   '<h2>Forms</h2><ul>',
+     'form':        '<li><a href="/<%= app._name %>/<%= name %>"><%= name %></a></li>',
+    'form_end':     '</ul>',
+    'view_start':   '<h2>Views</h2><ul>',
+     'view':        '<li><a href="/<%= app._name %>/<%= name %>"><%= name %></a></li>',
+    'view_end':     '</ul>'
+  }, { app: app }).t;
 
-  // Display all forms
-  html.push('<h2>Forms</h2><ul>');
-  _(app.form).each(function(form, name) {
-    html.push(_.template('<li><a href="/<%= app._name %>/<%= name %>"><%= name %></a></li>', {app: app, name:name}));
-  });
-  html.push('</ul>');
+  t('form_start');
+  _(app.form).each(function(form, name) { t('form', {form:form, name:name}); });
+  t('form_end');
+  t('view_start');
+  _(app.view).each(function(view, name) { t('view', {view:view, name:name}); });
+  t('view_end');
 
-  // Display all views
-  html.push('<h2>Views</h2><ul>');
-  _(app.view).each(function(view, name) {
-    html.push(_.template('<li><a href="/<%= app._name %>/<%= name %>"><%= name %></a></li>', {app: app, name:name}));
-  });
-  html.push('</ul>');
-
-  return html.join('');
+  return t().join('');
 };
 
-// Render a form in FDL
+// Render a form
 // ------------------------------------------------------------------------------------------------------------------------------------------------------
 this.form = function(app, formname, data, errors) {
   data = data || {};
   errors = errors || {};
-  var form = app.form[formname];
-
-  // Basic templates
+  var global = { app: app, form: app.form[formname] };
   var templates = {
-    // Start and end of a form.
     form_start:     '<form method="post">',
-    form_end:       '<button type="submit"><%= (form.actions || {}).submit || "Submit" %></button></form>',
+     doc_ref:       '<input type="hidden" name="_id" value="<%= doc._id %>"/><input type="hidden" name="doc._rev" value="<%= doc._rev %>"/>',
+     section_start: '<div class="section" id="<%= field.id %>"><h2><%= field.section %></h2><p><%= field.help %></p><fieldset><dl>',
+      label:        '<dt class="<%= error ? "error" : "" %>"><label for="<%= field.id %>"><%= field.label %></label><% if (field.help) { print("<p>", field.help, "</p>") } %></dt>',
+      input:        '<dd><input name="<%= field.id %>" id="<%= field.id %>" type="<%= field.type || "text" %>" value="<%= val %>"/></dd>',
+      radio:        '<dd><% for (var i=0,l=values.length; i<l; i++) { print("<label><input type=\'", field.type, "\' name=\'", field.id, "\' value=\'", values[i], "\'", values[i] === val ? " checked=\'checked\'" : "", " />", values[i], "</label>"); } %></dd>',
+      checkbox:     '<dd><% for (var i=0,l=values.length; i<l; i++) { print("<label><input type=\'", field.type, "\' name=\'", field.id, "\' value=\'", values[i], "\'", val.indexOf(values[i]) >= 0 ? " checked=\'checked\'" : "", " />", values[i], "</label>"); } %></dd>',
+      textarea:     '<dd><textarea name="<%= field.id %>" id="<%= field.id %>" type="<%= field.type || "text" %>"><%= val %></textarea></dd>',
+      select:       '<dd><select name="<%= field.id %>" id="<%= field.id %>"><% for (var i=0,l=values.length; i<l; i++) { print("<option", values[i]==val ? " selected=\'selected\'" : "", ">", values[i], "</option>"); } %></select></dd>',
 
-    // Start and end of a section
-    section_start:  '<div class="section" id="<%= field.id %>"><h2><%= field.section %></h2><% if (field.help) { print("<p>", field.help, "</p>") } %><fieldset><dl>',
-    section_end:    '</dl></fieldset></div>',
-    // This is used when the form starts directly with a field, not with a section
-    section_start0: '<div class="section"><fieldset><dl>',
-
-    // Start and end of a field
-    field_start:    '<dt class="<%= error ? "error" : "" %>"><label for="<%= field.id %>"><%= field.label %></label><% if (field.help) { print("<p>", field.help, "</p>") } %></dt><dd>',
-    field_end:      '</dd>',
-
-    // Field definitions for various field types. `input` is the generic catch-all
-    input:          '<input name="<%= field.id %>" id="<%= field.id %>" type="<%= field.type || "text" %>" value="<%= val %>"/>',
-    radio:          '<% for (var i=0,l=values.length; i<l; i++) { print("<label><input type=\'", field.type, "\' name=\'", field.id, "\' value=\'", values[i], "\'", values[i] === val ? " checked=\'checked\'" : "", " />", values[i], "</label>"); } %>',
-    checkbox:       '<% for (var i=0,l=values.length; i<l; i++) { print("<label><input type=\'", field.type, "\' name=\'", field.id, "\' value=\'", values[i], "\'", val.indexOf(values[i]) >= 0 ? " checked=\'checked\'" : "", " />", values[i], "</label>"); } %>',
-    textarea:       '<textarea name="<%= field.id %>" id="<%= field.id %>" type="<%= field.type || "text" %>"><%= val %></textarea>',
-    select:         '<select name="<%= field.id %>" id="<%= field.id %>"><% for (var i=0,l=values.length; i<l; i++) { print("<option", values[i]==val ? " selected=\'selected\'" : "", ">", values[i], "</option>"); } %></select>',
-
-    // Error messages
-    error:          '<span class="error"> <%= msg %></span>',
-
-    // Hidden fields
-    hidden:         '<input type="hidden" name="_id" value="<%= _id %>"/><input type="hidden" name="_rev" value="<%= _rev %>"/>'
+      error:        '<span class="error"> <%= msg %></span>',
+     section_end:    '</dl></fieldset></div>',
+    form_end:       '<button type="submit"><%= (form.actions || {}).submit || "Submit" %></button></form>'
   };
+  var t = new _.Template(templates, global).t;
 
-  // t('template-name', data) renders the template
-  var t = function(template, data) { return _.template(templates[template], data || {}); };
-
-  // html is an array that holds the output
-  var html = [];
-
-  html.push(t('form_start', {form:form}));
-  if (data && data._id) { html.push(t('hidden', data)); }
-  for (var i=0, field; field=form.fields[i]; i++) {
-    // Fields can be either a input definition or a section definition.
-    // If it's an input definition...
+  t('form_start');
+  if (data && data._id) { t('doc_ref', {doc:data}); }
+  _(global.form.fields).each(function(field, index) {
+    if (field.section || index === 0) {
+      if (index > 0) { t('section_end'); }
+      t('section_start', {field: field.section ? field : {} });
+    }
     if (field.label) {
-      // If the first field isn't a section, start with a blank section
-      if (i == 0) { html.push(t('section_start0')); }
-
-      // Pick the right template and use it
-      var tmpl = templates[field.type] ? field.type : 'input',
-          param = {
-            field:field,
-            val: data[field.id] || field.default || '',
-            values: (field.values && field.values.form && field.values.field) ? app._lookup(field.values.form, field.values.field) : field.values,
-            error:errors[field.id]
-          };
-      html.push(t('field_start', param));
-      html.push(t(tmpl, param));
-      _.each(param.error, function(e) { html.push(t('error', {msg:e})); });
-      html.push(t('field_end', param));
+      var err = errors[field.id];
+      t('label', {
+        field: field,
+        error: err,
+        val: data[field.id] || field.default || '',
+        values: (field.values && field.values.form && field.values.field) ? app._lookup(field.values.form, field.values.field) : field.values
+      });
+      t(templates[field.type] ? field.type : 'input');
+      _(err).each(function(e) { t('error', {msg:e}); });
     }
+  });
+  t('section_end');
+  t('form_end');
 
-    // If it's a section definition,
-    else if (field.section) {
-      // Close the previous section, except for the first field
-      if (i > 0) { html.push(t('section_end')); }
-
-      // Push the HTML for the section
-      html.push(t('section_start', {field:field}));
-    }
-  }
-
-  html.push(t('section_end'));
-  html.push(t('form_end', {form:form}));
-
-  return html.join('');
-};
+  return t().join('');
+}
 
 // Render a list of docs for a form into XHTML
 // ------------------------------------------------------------------------------------------------------------------------------------------------------
 this.view = function(app, viewname, docs) {
-  var view = app.view[viewname];
-
-  // Basic templates
-  var templates = {
-    view_header:     '<table><thead><tr><% for (var i=0, field; field=form.fields[i]; i++) { if (field.label) { print("<th>", field.label, "</th>"); } } %></tr></thead><tbody>',
-     view_row_header: '<tr>',
-      view_row_data:   '<td><a href="/<%= app._name %>/<%= view.form %>/<%= doc._id %>"><%= doc[field.id] %></a></td>',
-     view_row_footer: '</tr>',
-    view_footer:     '</tbody></table>',
-    action_header:   '<ul>',
-     action_row:      '<li><a href="/<%= app._name %><%= action.url %>"><%= action.text %></li>',
-    action_footer:   '</ul>'
+  var global = {
+    app: app,
+    view: app.view[viewname],
+    form: app.form[app.view[viewname].form]
   };
 
-  // t('template-name', data) renders the template
-  var t = function(template, data) { return _.template(templates[template], data || {}); };
+  // Basic templates
+  var t = new _.Template({
+    view_start:         '<table>',
 
-  var form = app.form[view.form];
-  var fieldlist = _(form.fields).select(function(field) { return field.label });
+     view_head_start:   '<thead><tr>',
+      view_head:        '<th><%= field.label %></th>',
+     view_head_end:     '</tr></thead><tbody>',
 
-  // html is an array that holds the output
-  var html = [];
-  html.push(t('view_header', {form:form}));
+     view_row_start:    '<tr>',
+      view_row:         '<td><a href="/<%= app._name %>/<%= view.form %>/<%= doc._id %>"><%= doc[field.id] %></a></td>',
+     view_row_end:      '</tr>',
+
+    view_end:           '</tbody></table>',
+
+    action_start:   '<ul>',
+     action_row:      '<li><a href="/<%= app._name %><%= action.url %>"><%= action.text %></li>',
+    action_end:   '</ul>'
+  }, global).t;
+
+  // Pick the fields that have labels
+  var fieldlist = _(global.form.fields).select(function(field) { return field.label });
+
+  t('view_start');
+  t('view_head_start');
+  _(fieldlist).each(function(field) { t('view_head', {field:field}); });
+  t('view_head_end');
+
   _(docs).each(function(doc) {
-    html.push(t('view_row_header'));
-    _(fieldlist).each(function(field) {
-      html.push(t('view_row_data', {app:app, view:view, doc:doc, field:field}));
-    });
-    html.push(t('view_row_footer'));
+    t('view_row_start', {doc:doc});
+    _(fieldlist).each(function(field) { t('view_row', {field:field}); });
+    t('view_row_end', global);
   });
-  // _.each(docs, function(doc) { html.push(t('doc_row', {form:form, doc:doc})); });
-  html.push(t('view_footer', {form:form}));
+  t('view_end');
 
-  html.push(t('action_header'));
-  _.each(view.actions, function(action) { html.push(t('action_row', {app:app, action:action})); })
-  html.push(t('action_footer'));
+  t('action_start');
+  _.each(global.view.actions, function(action) { t('action_row', {action:action}); })
+  t('action_end');
 
-  return html.join('');
+  return t().join('');
 };
 
 
