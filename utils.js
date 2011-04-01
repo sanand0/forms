b6f9902362d3bbde3149031e0b33873057d0621e
S Anand <root.node@gmail.com>
2011-04-01 11:47:24 +0100

    readFile reliably watches, and also handles deleted files

--- utils.js
+++ utils.js
@@ -1,21 +1,25 @@
 var fs = require('fs');
+var path = require('path');
 
 module.exports = {
   // Return the contents of a file, cache it, and periodically update it
   readFile: (function() {
     var cache = {};
     return function(filename) {
-      if (cache[filename]) { return cache[filename]; }
-      fs.watchFile(filename, function(curr, prev) {
-        if (curr.mtime <= prev.mtime) { return; }
-        fs.readFile(filename, function(err, data) {
-          if (err) { return; }
-          cache[filename] = data;
+      if (filename in cache) {
+        return cache[filename];
+      }
+      else {
+        fs.watchFile(filename, function(curr, prev) {
+          if (!curr.nlink) { cache[filename] = ''; }
+          if (curr.mtime <= prev.mtime) { return; }
+          fs.readFile(filename, 'utf-8', function(err, data) {
+            if (err) { return; }
+            cache[filename] = data;
+          });
         });
-      });
-      return cache[filename] = fs.readFileSync(filename, 'utf-8');
+      }
+      return cache[filename] = path.existsSync(filename) ? fs.readFileSync(filename, 'utf-8') : '';
     };
   })()
 };
-
-module.exports.readFile = function(filename) { return fs.readFileSync(filename, 'utf-8'); };
