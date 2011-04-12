32bd16f42ce05ec1aaa346d12552618c7697ca65
S Anand <root.node@gmail.com>
2011-04-12 09:39:05 +0100

    readFile works with multiple files as fallbacks

--- utils.js
+++ utils.js
@@ -1,26 +1,33 @@
+var _ = require('underscore');
 var fs = require('fs');
 var path = require('path');
 
 module.exports = {
-  // Return the contents of a file, cache it, and periodically update it
+  // readFile(file1, file2, ...) returns the contents of the first found file
+  // It also caches the contents, refreshing when the file changes
   readFile: (function() {
     var cache = {};
-    return function(filename) {
-      filename = path.join(__dirname, filename);
-      if (filename in cache) {
-        return cache[filename];
-      }
-      else {
-        fs.watchFile(filename, function(curr, prev) {
-          if (!curr.nlink) { cache[filename] = ''; }
-          if (curr.mtime <= prev.mtime) { return; }
-          fs.readFile(filename, 'utf-8', function(err, data) {
-            if (err) { return; }
-            cache[filename] = data;
-          });
+    var watching = {};
+    var watch = function(file) {
+      if (watching[file]) { return; }
+      watching[file] = fs.watchFile(file, function(curr, prev) {
+        if (!curr.nlink) { delete cache[file]; }
+        if (curr.mtime <= prev.mtime) { return; }
+        fs.readFile(file, 'utf-8', function(err, data) {
+          if (!err) { cache[file] = data; }
         });
+      });
+    };
+
+    return function() {
+      for (var i=0, file; file=arguments[i]; i++) {
+        file = path.resolve(__dirname, file);
+        watch(file);
+        if (file in cache) { return cache[file]; }
+        else if (path.existsSync(file)) { return cache[file] = fs.readFileSync(file, 'utf-8'); }
       }
-      return cache[filename] = path.existsSync(filename) ? fs.readFileSync(filename, 'utf-8') : '';
+
+      return '';
     };
   })()
 };
